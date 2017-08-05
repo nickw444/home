f4a4b43fb8e5d5d725e2766fc32140d7322cedb1
Nick Whyte <nick@nickwhyte.com>
2017-08-05 18:58:58 +1000

    Update deps

--- bridge/vendor/github.com/eclipse/paho.mqtt.golang/net.go
+++ bridge/vendor/github.com/eclipse/paho.mqtt.golang/net.go
@@ -125,7 +125,7 @@ func incoming(c *client) {
 		case c.ibound <- cp:
 			// Notify keepalive logic that we recently received a packet
 			if c.options.KeepAlive != 0 {
-				c.packetResp <- struct{}{}
+				c.packetResp.Broadcast()
 			}
 		case <-c.stop:
 			// This avoids a deadlock should a message arrive while shutting down.
@@ -205,11 +205,7 @@ func outgoing(c *client) {
 		}
 		// Reset ping timer after sending control packet.
 		if c.options.KeepAlive != 0 {
-			select {
-			case c.keepaliveReset <- struct{}{}:
-			default:
-				DEBUG.Println(NET, "couldn't send keepalive signal in outbound as channel full")
-			}
+			c.keepaliveReset.Broadcast()
 		}
 	}
 }
@@ -232,20 +228,22 @@ func alllogic(c *client) {
 			switch m := msg.(type) {
 			case *packets.PingrespPacket:
 				DEBUG.Println(NET, "received pingresp")
-				c.pingResp <- struct{}{}
+				c.pingResp.Broadcast()
 			case *packets.SubackPacket:
 				DEBUG.Println(NET, "received suback, id:", m.MessageID)
-				token := c.getToken(m.MessageID).(*SubscribeToken)
-				DEBUG.Println(NET, "granted qoss", m.ReturnCodes)
-				for i, qos := range m.ReturnCodes {
-					token.subResult[token.subs[i]] = qos
+				token := c.getToken(m.MessageID)
+				switch t := token.(type) {
+				case *SubscribeToken:
+					DEBUG.Println(NET, "granted qoss", m.ReturnCodes)
+					for i, qos := range m.ReturnCodes {
+						t.subResult[t.subs[i]] = qos
+					}
 				}
 				token.flowComplete()
 				c.freeID(m.MessageID)
 			case *packets.UnsubackPacket:
 				DEBUG.Println(NET, "received unsuback, id:", m.MessageID)
-				token := c.getToken(m.MessageID).(*UnsubscribeToken)
-				token.flowComplete()
+				c.getToken(m.MessageID).flowComplete()
 				c.freeID(m.MessageID)
 			case *packets.PublishPacket:
 				DEBUG.Println(NET, "received publish, msgId:", m.MessageID)
@@ -257,7 +255,10 @@ func alllogic(c *client) {
 					pr := packets.NewControlPacket(packets.Pubrec).(*packets.PubrecPacket)
 					pr.MessageID = m.MessageID
 					DEBUG.Println(NET, "putting pubrec msg on obound")
-					c.oboundP <- &PacketAndToken{p: pr, t: nil}
+					select {
+					case c.oboundP <- &PacketAndToken{p: pr, t: nil}:
+					case <-c.stop:
+					}
 					DEBUG.Println(NET, "done putting pubrec msg on obound")
 				case 1:
 					c.incomingPubChan <- m
@@ -265,10 +266,16 @@ func alllogic(c *client) {
 					pa := packets.NewControlPacket(packets.Puback).(*packets.PubackPacket)
 					pa.MessageID = m.MessageID
 					DEBUG.Println(NET, "putting puback msg on obound")
-					c.oboundP <- &PacketAndToken{p: pa, t: nil}
+					select {
+					case c.oboundP <- &PacketAndToken{p: pa, t: nil}:
+					case <-c.stop:
+					}
 					DEBUG.Println(NET, "done putting puback msg on obound")
 				case 0:
-					c.incomingPubChan <- m
+					select {
+					case c.incomingPubChan <- m:
+					case <-c.stop:
+					}
 					DEBUG.Println(NET, "done putting msg on incomingPubChan")
 				}
 			case *packets.PubackPacket:
@@ -283,7 +290,7 @@ func alllogic(c *client) {
 				prel.MessageID = m.MessageID
 				select {
 				case c.oboundP <- &PacketAndToken{p: prel, t: nil}:
-				case <-time.After(time.Second):
+				case <-c.stop:
 				}
 			case *packets.PubrelPacket:
 				DEBUG.Println(NET, "received pubrel, id:", m.MessageID)
@@ -291,7 +298,7 @@ func alllogic(c *client) {
 				pc.MessageID = m.MessageID
 				select {
 				case c.oboundP <- &PacketAndToken{p: pc, t: nil}:
-				case <-time.After(time.Second):
+				case <-c.stop:
 				}
 			case *packets.PubcompPacket:
 				DEBUG.Println(NET, "received pubcomp, id:", m.MessageID)
@@ -312,7 +319,7 @@ func errorWatch(c *client) {
 		return
 	case err := <-c.errors:
 		ERROR.Println(NET, "error triggered, stopping")
-		c.internalConnLost(err)
+		go c.internalConnLost(err)
 		return
 	}
 }
--- bridge/vendor/github.com/eclipse/paho.mqtt.golang/ping.go
+++ bridge/vendor/github.com/eclipse/paho.mqtt.golang/ping.go
@@ -16,6 +16,7 @@ package mqtt
 
 import (
 	"errors"
+	"sync"
 	"time"
 
 	"github.com/eclipse/paho.mqtt.golang/packets"
@@ -24,6 +25,18 @@ import (
 func keepalive(c *client) {
 	DEBUG.Println(PNG, "keepalive starting")
 
+	var condWG sync.WaitGroup
+	pingStop := make(chan struct{})
+
+	defer func() {
+		close(pingStop)
+		c.keepaliveReset.Broadcast()
+		c.pingResp.Broadcast()
+		c.packetResp.Broadcast()
+		condWG.Wait()
+		c.workers.Done()
+	}()
+
 	receiveInterval := c.options.KeepAlive + (1 * time.Second)
 	pingTimer := timer{Timer: time.NewTimer(c.options.KeepAlive)}
 	receiveTimer := timer{Timer: time.NewTimer(receiveInterval)}
@@ -31,25 +44,64 @@ func keepalive(c *client) {
 
 	pingRespTimer.Stop()
 
-	for {
-		select {
-		case <-c.stop:
-			DEBUG.Println(PNG, "keepalive stopped")
-			c.workers.Done()
-			return
-		case <-pingTimer.C:
-			sendPing(&pingTimer, &pingRespTimer, c)
-		case <-c.keepaliveReset:
-			DEBUG.Println(NET, "resetting ping timer")
-			pingTimer.Reset(c.options.KeepAlive)
-		case <-c.pingResp:
+	condWG.Add(3)
+	go func() {
+		defer condWG.Done()
+		for {
+			c.pingResp.L.Lock()
+			c.pingResp.Wait()
+			c.pingResp.L.Unlock()
+			select {
+			case <-pingStop:
+				return
+			default:
+			}
 			DEBUG.Println(NET, "resetting ping timeout timer")
 			pingRespTimer.Stop()
 			pingTimer.Reset(c.options.KeepAlive)
 			receiveTimer.Reset(receiveInterval)
-		case <-c.packetResp:
+		}
+	}()
+
+	go func() {
+		defer condWG.Done()
+		for {
+			c.packetResp.L.Lock()
+			c.packetResp.Wait()
+			c.packetResp.L.Unlock()
+			select {
+			case <-pingStop:
+				return
+			default:
+			}
 			DEBUG.Println(NET, "resetting receive timer")
 			receiveTimer.Reset(receiveInterval)
+		}
+	}()
+
+	go func() {
+		defer condWG.Done()
+		for {
+			c.keepaliveReset.L.Lock()
+			c.keepaliveReset.Wait()
+			c.keepaliveReset.L.Unlock()
+			select {
+			case <-pingStop:
+				return
+			default:
+			}
+			DEBUG.Println(NET, "resetting ping timer")
+			pingTimer.Reset(c.options.KeepAlive)
+		}
+	}()
+
+	for {
+		select {
+		case <-c.stop:
+			DEBUG.Println(PNG, "keepalive stopped")
+			return
+		case <-pingTimer.C:
+			sendPing(&pingTimer, &pingRespTimer, c)
 		case <-receiveTimer.C:
 			receiveTimer.SetRead(true)
 			receiveTimer.Reset(receiveInterval)
@@ -57,8 +109,7 @@ func keepalive(c *client) {
 		case <-pingRespTimer.C:
 			pingRespTimer.SetRead(true)
 			CRITICAL.Println(PNG, "pingresp not received, disconnecting")
-			c.workers.Done()
-			c.internalConnLost(errors.New("pingresp not received, disconnecting"))
+			c.errors <- errors.New("pingresp not received, disconnecting")
 			pingTimer.Stop()
 			return
 		}
@@ -66,16 +117,21 @@ func keepalive(c *client) {
 }
 
 type timer struct {
+	sync.Mutex
 	*time.Timer
 	readFrom bool
 }
 
 func (t *timer) SetRead(v bool) {
+	t.Lock()
 	t.readFrom = v
+	t.Unlock()
 }
 
 func (t *timer) Stop() bool {
+	t.Lock()
 	defer t.SetRead(true)
+	defer t.Unlock()
 
 	if !t.Timer.Stop() && !t.readFrom {
 		<-t.C
@@ -85,8 +141,13 @@ func (t *timer) Stop() bool {
 }
 
 func (t *timer) Reset(d time.Duration) bool {
+	t.Lock()
 	defer t.SetRead(false)
-	t.Stop()
+	defer t.Unlock()
+	if !t.Timer.Stop() && !t.readFrom {
+		<-t.C
+	}
+
 	return t.Timer.Reset(d)
 }
 
