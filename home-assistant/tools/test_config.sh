68fe8be2bfe36da778ca5d99bc1ba52a7e601f7b
Nick Whyte <nick@nickwhyte.com>
2019-09-23 20:17:21 +1000

    ha: Validate config in travis

---
+++ home-assistant/tools/test_config.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -euo pipefail
+set -x
+
+CONFIG_PATH=$(dirname "$0")/../
+
+main() {
+    local config_dir="${CONFIG_PATH}/$1";
+
+    cd "${config_dir}";
+
+    HA_VERSION=$(<.HA_VERSION)
+    mv secrets.example.yaml secrets.yaml
+
+    # pip3 install homeassistant==${HA_VERSION}
+    hass --script check_config -c . -f
+}
+
+main "$@"
