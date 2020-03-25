0cc99868850b478bc30aafc707dc301411387a63
Nick Whyte <nick@nickwhyte.com>
2020-03-25 22:17:05 +1100

    .

--- home-assistant/martin-pl/appdaemon/apps/media_player_light.py
+++ home-assistant/martin-pl/appdaemon/apps/media_player_light.py
@@ -26,15 +26,32 @@ class MediaPlayerLight(hass.Hass):
             self.args[CONF_MEDIA_PLAYER_ENTITY],
             attribute='entity_picture'
         )
+        current_entity_picture = self.get_state(
+            self.args[CONF_MEDIA_PLAYER_ENTITY],
+            attribute='entity_picture'
+        )
+        self.on_entity_picture_changed(current_entity_picture)
 
     def handle_update(self, entity, attribute, old, new, kwargs):
+        self.on_entity_picture_changed(new)
+
+    def on_entity_picture_changed(self, entity_picture):
         enable_source = self.args[CONF_ENABLE_SOURCE]
-        source = self.get_state(self.args[CONF_MEDIA_PLAYER_ENTITY], attribute="source")
-        if enable_source is not None and source != enable_source:
-            self.log('Spotify is not playing to the TV. Ignoring state change', level="INFO")
+        state = self.get_state(self.args[CONF_MEDIA_PLAYER_ENTITY])
+        if state != 'playing':
+            self.log(f'media player is not playing ({state}). Ignoring state change', level="INFO")
             return
 
-        artwork_url = self.get_fully_qualified_artwork_url(new)
+        source = self.get_state(self.args[CONF_MEDIA_PLAYER_ENTITY], attribute="source")
+        if enable_source is not None and source != enable_source:
+            self.log(
+                f'the current source ({source}) is not set to the enable source ({enable_source}). '
+                f'Ignoring state change',
+                level="INFO"
+            )
+            return
+
+        artwork_url = self.get_fully_qualified_artwork_url(entity_picture)
         rgb_color = extract_dominant_color(artwork_url)
         self.log(
             f'media player entity picture changed. Dominant color is: {rgb_color}',
