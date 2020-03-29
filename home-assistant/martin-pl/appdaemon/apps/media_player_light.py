f65f447835d053369cc1da32bdb0c73a0d9ba957
Nick Whyte <nick@nickwhyte.com>
2020-03-29 22:39:19 +1100

    .

--- home-assistant/martin-pl/appdaemon/apps/media_player_light.py
+++ home-assistant/martin-pl/appdaemon/apps/media_player_light.py
@@ -8,15 +8,15 @@ from colorthief import ColorThief
 # The source the media player needs to be set to in order for the color to be set. Set
 # this to your TV's device ID within Spotify (if using Spotify as your media player). Set
 # to null if the color should always be set.
-CONF_ENABLE_SOURCE = "enable_source"
+CONF_ENABLE_SOURCE = 'enable_source'
 # The URL to access the Home Assistant instance. Required in order to download the
 # artwork (entity_picture state attribute is a relative path)
-CONF_HASS_BASE_URL = "hass_base_url"
+CONF_HASS_BASE_URL = 'hass_base_url'
 # The entity ID of the media player to obtain the artwork from (using
 # entity_picture state attribute)
-CONF_MEDIA_PLAYER_ENTITY = "media_player_entity"
+CONF_MEDIA_PLAYER_ENTITY = 'media_player_entity'
 # The entity ID of the light to set the color
-CONF_LIGHT_ENTITY = "light_entity"
+CONF_LIGHT_ENTITY = 'light_entity'
 
 
 class MediaPlayerLight(hass.Hass):
@@ -24,10 +24,11 @@ class MediaPlayerLight(hass.Hass):
         self.listen_state(
             self.handle_update,
             self.args[CONF_MEDIA_PLAYER_ENTITY],
-            attribute="entity_picture",
+            attribute='entity_picture'
         )
         current_entity_picture = self.get_state(
-            self.args[CONF_MEDIA_PLAYER_ENTITY], attribute="entity_picture"
+            self.args[CONF_MEDIA_PLAYER_ENTITY],
+            attribute='entity_picture'
         )
         self.on_entity_picture_changed(current_entity_picture)
 
@@ -37,33 +38,30 @@ class MediaPlayerLight(hass.Hass):
     def on_entity_picture_changed(self, entity_picture):
         enable_source = self.args[CONF_ENABLE_SOURCE]
         state = self.get_state(self.args[CONF_MEDIA_PLAYER_ENTITY])
-        if state != "playing":
-            self.log(
-                f"media player is not playing ({state}). Ignoring state change",
-                level="INFO",
-            )
+        if state != 'playing':
+            self.log(f'media player is not playing ({state}). Ignoring state change', level="INFO")
             return
 
         source = self.get_state(self.args[CONF_MEDIA_PLAYER_ENTITY], attribute="source")
         if enable_source is not None and source != enable_source:
             self.log(
-                f"the current source ({source}) is not set to the enable source "
-                f" ({enable_source}). Ignoring state change",
-                level="INFO",
+                f'the current source ({source}) is not set to the enable source ({enable_source}). '
+                f'Ignoring state change',
+                level="INFO"
             )
             return
 
         artwork_url = self.get_fully_qualified_artwork_url(entity_picture)
         rgb_color = extract_dominant_color(artwork_url)
         self.log(
-            f"media player entity picture changed. Dominant color is: {rgb_color}",
-            level="INFO",
+            f'media player entity picture changed. Dominant color is: {rgb_color}',
+            level="INFO"
         )
 
         self.turn_on(self.args[CONF_LIGHT_ENTITY], rgb_color=rgb_color)
 
     def get_fully_qualified_artwork_url(self, image_path):
-        return urljoin(self.args["hass_base_url"], image_path)
+        return urljoin(self.args['hass_base_url'], image_path)
 
 
 def extract_dominant_color(image_url):
