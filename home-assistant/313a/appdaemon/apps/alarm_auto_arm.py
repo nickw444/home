407238adafe192c2300d6b8ac70ccb76e85a9ead
Nick Whyte <nick@nickwhyte.com>
2020-03-29 22:43:22 +1100

    Add appdaemon app for 313a alarm auto-arm + tests (#75)

---
+++ home-assistant/313a/appdaemon/apps/alarm_auto_arm.py
@@ -0,0 +1,89 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+CONF_ALARM_ENTITY = "alarm_entity"
+CONF_ARMING_CODE = "arming_code"
+CONF_PRESENCE_ENTITY = "presence_entity"
+CONF_ENABLE_ENTITY = "enable_entity"
+CONF_ENABLE_OVERRIDE_ENTITY = "enable_override_entity"
+
+
+class AlarmAutoArm(hass.Hass):
+    """
+    When:
+        * Everyone leaves
+        * Auto-arm re-enabled (via schedule)
+        * When enable input boolean re-enabled
+    Conditions:
+        * Panel not already armed
+        * No one is home
+        * Input boolean is enabled
+    Then:
+        * Arm
+
+    When:
+        * Someone arrives home
+        * Auto-arm is disabled (via schedule)
+        * When input boolean disabled
+    Conditions:
+        * Panel is armed
+    Then:
+        * Disarm the alarm panel
+    """
+
+    def initialize(self):
+        self.listen_state(
+            self.on_presence_state_change, self.args[CONF_PRESENCE_ENTITY]
+        )
+        self.listen_state(self.on_enable_change, self.args[CONF_ENABLE_ENTITY])
+        self.listen_state(self.on_enable_change, self.args[CONF_ENABLE_OVERRIDE_ENTITY])
+
+    def on_presence_state_change(self, entity, attribute, old, new, kwargs):
+        if new == "not_home":
+            self._maybe_arm()
+        else:
+            self._maybe_disarm()
+
+    def on_enable_change(self, entity, attribute, old, new, kwargs):
+        if new == "on":
+            self._maybe_arm()
+        else:
+            self._maybe_disarm()
+
+    def _maybe_arm(self):
+        if not self._can_arm():
+            return
+
+        self.call_service(
+            "alarm_control_panel.alarm_arm_away", entity_id=self.args[CONF_ALARM_ENTITY]
+        )
+        self.fire_event("auto_arm_armed")
+
+    def _maybe_disarm(self):
+        if not self._can_disarm():
+            return
+
+        self.call_service(
+            "alarm_control_panel.alarm_disarm",
+            entity_id=self.args[CONF_ALARM_ENTITY],
+            code=self.args[CONF_ARMING_CODE],
+        )
+        self.fire_event("auto_arm_disarmed")
+
+    def _can_arm(self):
+        alarm_state = self.get_state(self.args[CONF_ALARM_ENTITY])
+        presence_state = self.get_state(self.args[CONF_PRESENCE_ENTITY])
+        enable_state = self.get_state(self.args[CONF_ENABLE_ENTITY])
+        return (
+            alarm_state == "disarmed"
+            and presence_state == "not_home"
+            and enable_state == "on"
+            and self._is_enabled()
+        )
+
+    def _can_disarm(self):
+        alarm_state = self.get_state(self.args[CONF_ALARM_ENTITY])
+
+        return alarm_state == "armed_away" and self._is_enabled()
+
+    def _is_enabled(self):
+        return self.get_state(self.args[CONF_ENABLE_OVERRIDE_ENTITY]) == "on"
---
+++ home-assistant/313a/appdaemon/tests/test_alarm_auto_arm.py
@@ -0,0 +1,207 @@
+import mock
+import pytest
+
+from apps.alarm_auto_arm import (
+    AlarmAutoArm,
+    CONF_ALARM_ENTITY,
+    CONF_ARMING_CODE,
+    CONF_ENABLE_ENTITY,
+    CONF_ENABLE_OVERRIDE_ENTITY,
+    CONF_PRESENCE_ENTITY,
+)
+
+ALARM_ENTITY = "alarm_control_panel.alarm"
+ARMING_CODE = "123456"
+ENABLE_ENTITY = "input_boolean.enable"
+ENABLE_OVERRIDE_ENTITY = "input_boolean.enable_override"
+PRESENCE_ENTITY = "group.people"
+
+
+def test_callbacks_are_registered(hass_driver, auto_arm):
+    listen_state = hass_driver.get_mock("listen_state")
+    assert listen_state.call_count == 3
+    listen_state.assert_has_calls(
+        [
+            mock.call(auto_arm.on_presence_state_change, PRESENCE_ENTITY),
+            mock.call(auto_arm.on_enable_change, ENABLE_ENTITY),
+            mock.call(auto_arm.on_enable_change, ENABLE_OVERRIDE_ENTITY),
+        ]
+    )
+
+
+def test_when_everyone_leaves_then_arm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "home")
+        hass_driver.set_state(ALARM_ENTITY, "disarmed")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_called_once_with(
+        "alarm_control_panel.alarm_arm_away", entity_id=ALARM_ENTITY
+    )
+
+
+def test_when_everyone_leaves_and_not_enabled_then_do_not_arm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "home")
+        hass_driver.set_state(ALARM_ENTITY, "disarmed")
+        hass_driver.set_state(ENABLE_ENTITY, "off")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_not_called()
+
+
+def test_when_everyone_leaves_and_not_enabled_via_override_then_do_not_arm(
+    hass_driver, auto_arm
+):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "home")
+        hass_driver.set_state(ALARM_ENTITY, "disarmed")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "off")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_not_called()
+
+
+def test_when_everyone_leaves_and_armed_do_not_arm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "home")
+        hass_driver.set_state(ALARM_ENTITY, "armed_away")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_not_called()
+
+
+def test_when_re_enabled_then_arm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "disarmed")
+        hass_driver.set_state(ENABLE_ENTITY, "off")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(ENABLE_ENTITY, "on")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_called_once_with(
+        "alarm_control_panel.alarm_arm_away", entity_id=ALARM_ENTITY
+    )
+
+
+def test_when_override_re_enabled_then_arm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "disarmed")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "off")
+
+    hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_called_once_with(
+        "alarm_control_panel.alarm_arm_away", entity_id=ALARM_ENTITY
+    )
+
+
+def test_when_re_enabled_but_override_disabled_then_do_not_arm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "disarmed")
+        hass_driver.set_state(ENABLE_ENTITY, "off")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "off")
+
+    hass_driver.set_state(ENABLE_ENTITY, "on")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_not_called()
+
+
+def test_when_someone_home_disarm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "armed_away")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_called_once_with(
+        "alarm_control_panel.alarm_disarm", entity_id=ALARM_ENTITY, code=ARMING_CODE
+    )
+
+
+def test_fires_event_when_disarmed(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "armed_away")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "home")
+
+    fire_event = hass_driver.get_mock("fire_event")
+    fire_event.assert_called_once_with("auto_arm_disarmed")
+
+
+def test_when_someone_home_but_override_disabled_dont_disarm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "armed_away")
+        hass_driver.set_state(ENABLE_ENTITY, "on")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "off")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_not_called()
+
+
+def test_when_someone_home_but_disabled_then_disarm(hass_driver, auto_arm):
+    with hass_driver.setup():
+        hass_driver.set_state(PRESENCE_ENTITY, "not_home")
+        hass_driver.set_state(ALARM_ENTITY, "armed_away")
+        hass_driver.set_state(ENABLE_ENTITY, "off")
+        hass_driver.set_state(ENABLE_OVERRIDE_ENTITY, "on")
+
+    hass_driver.set_state(PRESENCE_ENTITY, "home")
+
+    call_service = hass_driver.get_mock("call_service")
+    call_service.assert_called_once_with(
+        "alarm_control_panel.alarm_disarm", entity_id=ALARM_ENTITY, code=ARMING_CODE
+    )
+
+
+@pytest.fixture
+def auto_arm():
+    ad = mock.Mock()
+    logging = mock.Mock()
+    auto_arm = AlarmAutoArm(
+        ad,
+        "name",
+        logging,
+        {
+            CONF_ALARM_ENTITY: ALARM_ENTITY,
+            CONF_ARMING_CODE: ARMING_CODE,
+            CONF_ENABLE_ENTITY: ENABLE_ENTITY,
+            CONF_ENABLE_OVERRIDE_ENTITY: ENABLE_OVERRIDE_ENTITY,
+            CONF_PRESENCE_ENTITY: PRESENCE_ENTITY,
+        },
+        {},
+        {},
+        {},
+    )
+    auto_arm.initialize()
+    return auto_arm
