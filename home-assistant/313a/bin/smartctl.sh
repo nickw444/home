6762d590bb7a4c38d55a101bb9a83c74833c420e
Nick Whyte <nick@nickwhyte.com>
2020-07-19 18:56:39 +1000

    .

--- home-assistant/313a/bin/smartctl.sh
+++ home-assistant/313a/bin/smartctl.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 CONFIG_DIR=$(cd "$(dirname "$0")"/.. && pwd)
 
-for arg in sd{a..m}; do
+for arg in sd{a..c}; do
     /usr/sbin/smartctl --info --all --json  \
         --nocheck standby /dev/$arg > "${CONFIG_DIR}/smartctl/$arg.json";
 done
