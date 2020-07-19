9720a0c6eb04dfc0672c9663b3d9198fd58c6219
Nick Whyte <nick@nickwhyte.com>
2020-07-19 22:12:24 +1000

    SMART HDD monitoring + system monitor (#88)

--- home-assistant/tools/test_config.sh
+++ home-assistant/tools/test_config.sh
@@ -8,12 +8,15 @@ main() {
 
     cd "${config_dir}";
 
-    HA_VERSION=$(cat .HA_VERSION)
-    cp secrets.example.yaml secrets.yaml
+    HA_VERSION=$(cat .HA_VERSION);
+    cp secrets.example.yaml secrets.yaml;
     echo "Installing homeassistant==${HA_VERSION}";
-    pip3 install "homeassistant==${HA_VERSION}"
+    pip3 install "homeassistant==${HA_VERSION}";
 
-    hass --script check_config -c . -f
+    # Workaround to "not a directory @ data['whitelist_external_dirs'][0]" failure in CI
+    sudo mkdir /config;
+
+    hass --script check_config -c . -f;
 }
 
 main "$@"
