21d3f8f2b0b550d3023e6398b17583fb4e49ed99
Nick Whyte <nick@nickwhyte.com>
2021-01-28 22:18:44 +1100

    Remove defunct integrations

--- home-assistant/313a/bin/smartctl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-CONFIG_DIR=$(cd "$(dirname "$0")"/.. && pwd)
-
-for arg in sd{a..c}; do
-    /usr/sbin/smartctl --info --all --json  \
-        --nocheck standby /dev/$arg > "${CONFIG_DIR}/smartctl/$arg.json";
-done
