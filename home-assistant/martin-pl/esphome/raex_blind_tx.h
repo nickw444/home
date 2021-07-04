884710cf16d258af46df5377ead27de07783f7d4
Nick Whyte <nick@nickwhyte.com>
2021-07-04 17:09:39 +1000

    mpl: tweak blind code tx retries

--- home-assistant/martin-pl/esphome/raex_blind_tx.h
+++ home-assistant/martin-pl/esphome/raex_blind_tx.h
@@ -1,8 +1,8 @@
 #include "esphome.h"
 
 #define TX_PIN 3
-#define TRANSMIT_RETRIES 15
-#define TRANSMIT_RETRY_DELAY_MS 1000
+#define TRANSMIT_RETRIES 10
+#define TRANSMIT_RETRY_DELAY_MS 3000
 #define CLOCK_WIDTH 330
 
 static const char *TAG = "raex_blind_tx";
