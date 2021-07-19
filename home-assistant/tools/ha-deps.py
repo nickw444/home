3d6f95803e1f8b0a04c9c1923ebdf269443fad49
Nick Whyte <nick@nickwhyte.com>
2021-07-19 21:50:53 +1000

    support fetching locked deps

--- home-assistant/tools/ha-deps.py
+++ home-assistant/tools/ha-deps.py
@@ -104,43 +104,65 @@ def write_locked_dependencies(path: str, locked_dependencies: Dict[
         yaml.dump(dumpable, f)
 
 
+def checkout_dependency_source(dependency: Dependency, ref: str = None):
+    tmpdir = tempfile.TemporaryDirectory(suffix='-' + dependency.get_name())
+    subprocess.run(
+        ['git', 'clone', dependency.source, tmpdir.name],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL)
+    if ref is not None:
+        subprocess.run(
+            ['git', 'checkout', ref],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            cwd=tmpdir.name)
+
+    return tmpdir
+
+
 def install_dependency(config_root_path: str, dependency: Dependency,
                        lock_info: LockedDependency):
-    click.echo(click.style(f'Installing: {dependency.get_name()} ', fg='green'))
+    click.echo(
+        click.style(f'Installing: {dependency.get_name()} ', fg='green'))
 
-    with tempfile.TemporaryDirectory(
-            suffix='-' + dependency.get_name()) as cloned_path:
-        subprocess.run(
-            ['git', 'clone', dependency.source, cloned_path],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL)
-
-        if maybe_install_core_dependency(config_root_path, dependency, cloned_path):
-            describe_result = subprocess.run(
-                ['git', 'describe', '--always'],
-                cwd=cloned_path, stdout=subprocess.PIPE)
-
-            return LockedDependency(
-                source=dependency.source,
-                version=describe_result.stdout.decode('utf-8').strip(),
-                is_release=False,
-                type='core'
-            )
-
-        # not core dependencies, try Lovelace dependencies
-        return maybe_install_lovelace_dependency(config_root_path, dependency, cloned_path)
+    if lock_info is not None and lock_info.type == 'lovelace' and lock_info.is_release:
+        # Install directly from Github Releases, skip inference logic.
+        return install_lovelace_release_dependency(
+            config_root_path, dependency, tag_name=lock_info.version)
+    else:
+        version_ref = lock_info.version if lock_info else None
+        with checkout_dependency_source(dependency,
+                                        version_ref) as source_path:
+            is_core = lock_info.type == 'core' if lock_info else is_core_dependency(
+                dependency, source_path)
+            if is_core:
+                return install_core_dependency(config_root_path, dependency,
+                                               source_path)
+            else:
+                return install_lovelace_dependency(
+                    config_root_path, dependency, source_path)
 
 
-def maybe_install_core_dependency(
-        config_root_path: str,
+def is_core_dependency(
         dependency: Dependency,
         cloned_path: str) -> bool:
     custom_components_path = os.path.join(cloned_path, 'custom_components')
     if dependency.root_is_custom_components:
         custom_components_path = cloned_path
 
+    return os.path.exists(custom_components_path)
+
+
+def install_core_dependency(
+        config_root_path: str,
+        dependency: Dependency,
+        cloned_path: str):
+    custom_components_path = os.path.join(cloned_path, 'custom_components')
+    if dependency.root_is_custom_components:
+        custom_components_path = cloned_path
+
     if not os.path.exists(custom_components_path):
-        return False
+        raise ArtifactNotFoundException()
 
     for component in os.listdir(custom_components_path):
         component_path = os.path.join(custom_components_path, component)
@@ -150,19 +172,26 @@ def maybe_install_core_dependency(
         if dependency.include is not None and component not in dependency.include:
             continue
 
-        destination_path = os.path.join(config_root_path, 'custom_components', component)
+        destination_path = os.path.join(config_root_path, 'custom_components',
+                                        component)
         if os.path.exists(destination_path):
             shutil.rmtree(destination_path)
 
         shutil.copytree(component_path, destination_path)
         click.echo(f"installed {component}")
 
-    return True
-
+    describe_result = subprocess.run(
+        ['git', 'describe', '--always'],
+        cwd=cloned_path, stdout=subprocess.PIPE)
+    return LockedDependency(
+        source=dependency.source,
+        version=describe_result.stdout.decode('utf-8').strip(),
+        is_release=False,
+        type='core'
+    )
 
 class ArtifactNotFoundException(Exception):
     pass
-
 
 
 def find_source_artifacts(dependency: Dependency, cloned_path: str,
@@ -175,17 +204,21 @@ def find_source_artifacts(dependency: Dependency, cloned_path: str,
 
     return artifacts
 
-def get_github_release(dependency: Dependency, ref: Optional[str]):
+
+def get_github_release(dependency: Dependency, tag_name: Optional[str]):
     github_slug = dependency.get_github_slug()
     if github_slug is None:
         # Cannot check Github if the dependency doesn't come from Github!
         return None
 
-    resp = requests.get(
-        f'https://api.github.com/repos/{github_slug}/releases/latest'
-    )
+    url = f'https://api.github.com/repos/{github_slug}/releases/latest'
+    if tag_name is not None:
+        url = f'https://api.github.com/repos/{github_slug}/releases/tags/{tag_name}'
+
+    resp = requests.get(url)
     resp.raise_for_status()
     return resp.json()
+
 
 def find_github_releases_artifacts(dependency: Dependency, release_data):
     artifacts = []
@@ -195,13 +228,45 @@ def find_github_releases_artifacts(dependency: Dependency, release_data):
 
     return artifacts
 
-def install_lovelace_asset(dependency: Dependency, path: str):
-    pass
 
-def maybe_install_lovelace_dependency(
+def install_lovelace_release_dependency(
         config_root_path: str,
         dependency: Dependency,
-        cloned_path: str,
+        tag_name: Optional[str]
+):
+    release_data = get_github_release(dependency, tag_name=tag_name)
+    github_artifacts = find_github_releases_artifacts(dependency, release_data)
+    if len(github_artifacts):
+        destination_path = os.path.join(
+            config_root_path, 'www', dependency.get_name())
+        if os.path.isdir(destination_path):
+            shutil.rmtree(destination_path)
+        os.mkdir(destination_path)
+
+        for artifact in github_artifacts:
+            artifact_basename = os.path.basename(urlparse(artifact).path)
+            artifact_destination_path = os.path.join(destination_path,
+                                                     artifact_basename)
+            resp = requests.get(artifact)
+            resp.raise_for_status()
+            with open(artifact_destination_path, 'wb') as f:
+                f.write(resp.content)
+            print(f"installed {artifact_basename}")
+
+        return LockedDependency(
+            source=dependency.source,
+            version=release_data['tag_name'],
+            is_release=True,
+            type='lovelace',
+        )
+
+    raise ArtifactNotFoundException()
+
+
+def install_lovelace_dependency(
+        config_root_path: str,
+        dependency: Dependency,
+        cloned_path: Optional[str],
 ) -> LockedDependency:
     hacs_json_path = os.path.join(cloned_path, 'hacs.json')
     if os.path.exists(hacs_json_path):
@@ -234,32 +299,8 @@ def maybe_install_lovelace_dependency(
             )
 
     # No source candidates found, try check Github Releases.
-    release_data = get_github_release(dependency, ref=None)
-    github_artifacts = find_github_releases_artifacts(dependency, release_data)
-    if len(github_artifacts):
-        destination_path = os.path.join(
-            config_root_path, 'www', dependency.get_name())
-        if os.path.isdir(destination_path):
-            shutil.rmtree(destination_path)
-        os.mkdir(destination_path)
-
-        for artifact in github_artifacts:
-            artifact_basename = os.path.basename(urlparse(artifact).path)
-            artifact_destination_path = os.path.join(destination_path, artifact_basename)
-            resp = requests.get(artifact)
-            resp.raise_for_status()
-            with open(artifact_destination_path, 'wb') as f:
-                f.write(resp.content)
-            print(f"installed {artifact_basename}")
-
-        return LockedDependency(
-            source=dependency.source,
-            version=release_data['tag_name'],
-            is_release=True,
-            type='lovelace',
-        )
-
-    raise ArtifactNotFoundException()
+    return install_lovelace_release_dependency(config_root_path, dependency,
+                                               None)
 
 
 @cli.command(help="Install dependencies from hass-deps.yaml")
@@ -270,15 +311,20 @@ def install(ctx):
     dependencies_path = os.path.join(config_dir, "hass-deps.yaml")
     dependencies_lock_path = os.path.join(config_dir, "hass-deps.lock")
     dependencies = load_dependencies(dependencies_path)
-    dependencies_lock = {}
+    locked_deps = {}
     if os.path.exists(dependencies_lock_path):
-        dependencies_lock = load_locked_dependencies(dependencies_lock_path)
+        locked_deps = load_locked_dependencies(dependencies_lock_path)
 
     for dependency in dependencies:
-        lock_info = install_dependency('./demo-config', dependency, None)
-        dependencies_lock[dependency.source] = lock_info
+        lock_info = locked_deps.get(dependency.source)
+        updated_lock_info = install_dependency(
+            './demo-config', dependency, lock_info)
 
-    write_locked_dependencies(dependencies_lock_path, dependencies_lock)
+        if lock_info is None:
+            # Only update locked dependency if no lock was previously specified
+            locked_deps[dependency.source] = updated_lock_info
+
+    write_locked_dependencies(dependencies_lock_path, locked_deps)
 
 
 @cli.command(help="Upgrade dependencies to the latest version/release")
