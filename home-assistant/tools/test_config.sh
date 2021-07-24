fa7c618ac35afd961fd76d5e3f4785c11e95b95e
Nick Whyte <nick@nickwhyte.com>
2021-07-24 15:51:57 +1000

    Manage 3rd party custom_components and lovelace cards with hass-deps (#97)

--- home-assistant/tools/test_config.sh
+++ home-assistant/tools/test_config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 CONFIG_PATH=$(dirname "$0")/../
@@ -11,7 +11,10 @@ main() {
     HA_VERSION=$(cat .HA_VERSION);
     cp secrets.example.yaml secrets.yaml;
     echo "Installing homeassistant==${HA_VERSION}";
-    pip3 install "homeassistant==${HA_VERSION}";
+    pip3 install hass-deps "homeassistant==${HA_VERSION}";
+    if [[ -f "hass-deps.yaml" ]]; then
+      hass-deps install
+    fi
 
     # Workaround to "not a directory @ data['whitelist_external_dirs'][0]" failure in CI
     sudo mkdir -p /config;
