07ae0a6295256ebd9e7ab2ecae50a14a6233fa17
Nick Whyte <nick@nickwhyte.com>
2021-07-24 15:42:31 +1000

    .

--- home-assistant/tools/test_config.sh
+++ home-assistant/tools/test_config.sh
@@ -11,7 +11,10 @@ main() {
     HA_VERSION=$(cat .HA_VERSION);
     cp secrets.example.yaml secrets.yaml;
     echo "Installing homeassistant==${HA_VERSION}";
-    pip3 install "homeassistant==${HA_VERSION}";
+    pip3 install hass-deps "homeassistant==${HA_VERSION}";
+    if [[ -f "hass-deps.yaml" ]]; then
+      hass-deps install
+    fi
 
     # Workaround to "not a directory @ data['whitelist_external_dirs'][0]" failure in CI
     sudo mkdir -p /config;
